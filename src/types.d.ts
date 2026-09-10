afec858ac41bd165ec9b841d4877a85559d7cbda
Silvan <silvan.zuend@protonmail.com>
2026-09-10 18:37:40 +0200

    Added Seebrücke image

--- src/types.d.ts
+++ src/types.d.ts
@@ -243,6 +243,8 @@ export interface Steps extends Omit<Headline, 'classes'>, Widget {
 export interface Content extends Omit<Headline, 'classes'>, Widget {
   content?: string;
   image?: string | unknown;
+  secondaryImage?: string | unknown;
+  secondaryImageCaption?: string;
   items?: Array<Item>;
   columns?: number;
   isReversed?: boolean;
